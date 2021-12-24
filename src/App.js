d1fc45598850997f3b2a0fa9a07bd6ed0a922d38
Laura <laurafcanon@gmail.com>
2021-12-24 10:07:15 -0500

    Adds routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/about/About";
+import Footer from "./Footer";
+import HomeLanding from "./components/landing/Landing";
+import ProductCard from "./components/products/Products";
+import ProductDetail from "./components/product_detail/ProductDetail";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="counter">
+          <ul className="nav-links">
+            <li className="list-prod">
+              <Link className="links" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="list-prod">
+              <Link className="links" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="list-prod">
+              <Link className="links" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route path="/" element={<HomeLanding />}></Route>
+          <Route path="/products" element={<ProductCard />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/products" element={<ProductDetail />}></Route>
+          <Route
+            path="/products/detail/:id"
+            element={<ProductDetail />}
+          ></Route>
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
