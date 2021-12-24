d8405d29e540090d4ac1089d1287a078952c9d00
Laura <laurafcanon@gmail.com>
2021-12-24 10:04:19 -0500

    Adds footer/style

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="container">
+      <div className="cont-foot">
+        <span className="copy">
+          &copy; 2021 <a href="https://github.com/LauraCanon"> LauraCanon</a> -
+          All rights reserved
+        </span>
+      </div>
+    </div>
+  );
+}
